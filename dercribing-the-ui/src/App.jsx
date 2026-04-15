ab2dbd1c961f38c7eb49b886821c383ba887d624
Jahid Hasan <jahidjubaer17@gmail.com>
2026-04-15 17:21:34 +0600

    dercribing the ui

---
+++ dercribing-the-ui/src/App.jsx
@@ -0,0 +1,12 @@
+import DrinkList from "./components/DrinkList";
+import PackingList from "./components/PackingList";
+
+function App() {
+  return (
+    <div>
+     <DrinkList></DrinkList>
+    </div>
+  );
+}
+
+export default App;
