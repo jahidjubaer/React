76419fd360d02482fb7e33050bdbd45c3cfce303
Jahid Hasan <jahidjubaer17@gmail.com>
2026-04-16 08:31:31 +0600

    rendering list

--- dercribing-the-ui/src/App.jsx
+++ dercribing-the-ui/src/App.jsx
@@ -1,10 +1,12 @@
 import DrinkList from "./components/DrinkList";
 import PackingList from "./components/PackingList";
+import People from "./components/People";
 
 function App() {
   return (
     <div>
-     <DrinkList></DrinkList>
+     {/* <DrinkList></DrinkList> */}
+     <People></People>
     </div>
   );
 }
